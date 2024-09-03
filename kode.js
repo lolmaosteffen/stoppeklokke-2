f3b57c6c85d1e897726badb4d0a83f0586d0976d
lolmaosteffen <steffen-1997@live.no>
2024-09-03 13:15:46 +0200

    funksjonell stoppeklokke lohl

--- kode.js
+++ kode.js
@@ -1,37 +1,49 @@
 const display = document.getElementById("display");
+const start = document.getElementById("startBtn");
+const stop = document.getElementById("stopBtn");
+const restart = document.getElementById("restartBtn");
+const lap = document.getElementById("lapBtn");
+const buttons = document.getElementById("buttons");
+const teksts = document.getElementById("tekst");
 let Timer = null;
 let startTime = 0;
 let elapsedTime = 0;
-let isRunning = false;
-
-
 
 function minTimer() {
-    Timer = setInterval(startButton, 1000)
+    Timer = setInterval(time, 1000)
     console.log(minTimer)
 }
+function time() {
+    elapsedTime += 1;
+    display.innerHTML = startTime + elapsedTime;
+}
 function startButton() {
-    document.getElementById("buttons").innerHTML = /*HTML*/`
+    buttons.innerHTML = /*HTML*/`
         <button onclick="stopButton()" id="stopBtn">stop</button>
         <button onclick="lapButton()" id="lapBtn">lap</button>
         <button onclick="restartButton()" id="restartBtn">restart</button>`;
-    // Timer += 1;
-    document.getElementById("display").innerHTML = Timer
     minTimer()
+    // Timer += 1;
+    // document.getElementById("display").innerHTML = Timer
 }
 function stopButton() {
-    document.getElementById("buttons").innerHTML = /*HTML*/`
+    buttons.innerHTML = /*HTML*/`
     <button id="startBtn" onclick="startButton()">start</button>
     <button id="stopBtn" onclick="stopButton()">stop</button>
-    <button id="restartBtn" onclick="restartButton()">reset</button>`;
-    clearInterval(minTimer)
+    <button id="restartBtn" onclick="restartButton()">restart</button>`;
+    clearInterval(Timer);
 }
 function restartButton() {
-    document.getElementById("buttons").innerHTML = /*HTML*/`
-        <button onclick="stopButton()" id="stopBtn">stop</button>
-        <button onclick="lapButton()" id="lapBtn">lap</button>
+    buttons.innerHTML = /*HTML*/`
+        <button onclick="stopButton()" id="startBtn">start</button>
+        <button onclick="lapButton()" id="stopBtn">stop</button>
         <button onclick="restartButton()" id="restartBtn">restart</button>`;
+    clearInterval(Timer);
+    elapsedTime = 0;
+    startTime = 0;
+    display.innerHTML = startTime;
 }
 function lapButton() {
-
+    teksts.innerHTML += ' time: ' + display.innerHTML;
 }
+console.log(minTimer)
